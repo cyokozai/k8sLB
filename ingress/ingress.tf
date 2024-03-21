2c758c280689385d3d6ef8002d4f83be342321cc
cyokozai <111193076+cyokozai@users.noreply.github.com>
2024-03-22 02:39:14 +0900

    20240322

--- ingress/ingress.tf
+++ ingress/ingress.tf
@@ -3,29 +3,29 @@ provider "kubernetes" {
 }
 
 
-resource "kubernetes_ingress" "http" {
-    metadata {
-        name = "minimal-ingress"
-        annotations = {
-            "nginx.ingress.kubernetes.io/rewrite-target" = "/"
-        }
-    }
+# resource "kubernetes_ingress" "http" {
+#     metadata {
+#         name = "minimal-ingress"
+#         annotations = {
+#             "nginx.ingress.kubernetes.io/rewrite-target" = "/"
+#         }
+#     }
 
-    spec {
-        Remove the ingress_class attribute
-        ingress_class = "nginx-example"
+#     spec {
+#         Remove the ingress_class attribute
+#         ingress_class = "nginx-example"
 
-        rule {
-            http {
-                path {
-                    path = "/testpath"
-                    path_type = "Prefix"
-                }
-                backend {
-                    service_name = "test"
-                    service_port = 443
-                }
-            }
-        }
-    }
-}
+#         rule {
+#             http {
+#                 path {
+#                     path = "/testpath"
+#                     path_type = "Prefix"
+#                 }
+#                 backend {
+#                     service_name = "test"
+#                     service_port = 443
+#                 }
+#             }
+#         }
+#     }
+# }
